708f95282dc549bb5aa8b0f185ebf5bc48e49a21
kamal <kamalkrami4@gmail.com>
2024-11-07 19:22:24 +0100

    Sixth Commit v1: Add Angular Frontend, Frontend Dockerfile, Configuration, and Docker Compose

---
+++ angular-front-end/src/app/accounts/accounts.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+@Component({
+  selector: 'app-accounts',
+  standalone: true,
+  imports: [HttpClientModule],
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css'
+})
+export class AccountsComponent implements OnInit{
+  accounts:any;
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:8888/ACCOUNT-SERVICE/accounts")
+      .subscribe({
+        next: data => {
+            this.accounts = data;
+        },
+        error : err => {
+          console.log(err);
+        }
+      })
+  }
+}
